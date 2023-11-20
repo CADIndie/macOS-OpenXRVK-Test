c14d744294c21c5838c41c71d0c46aa5a76debef
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2023-11-20 17:12:17 -0500

    Bruh -_-

---
+++ build/CMakeFiles/openxr-example.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Buffer.cpp" "CMakeFiles/openxr-example.dir/src/Buffer.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Buffer.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Context.cpp" "CMakeFiles/openxr-example.dir/src/Context.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Context.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Headset.cpp" "CMakeFiles/openxr-example.dir/src/Headset.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Headset.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Main.cpp" "CMakeFiles/openxr-example.dir/src/Main.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Main.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/MirrorView.cpp" "CMakeFiles/openxr-example.dir/src/MirrorView.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/MirrorView.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Pipeline.cpp" "CMakeFiles/openxr-example.dir/src/Pipeline.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Pipeline.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/RenderProcess.cpp" "CMakeFiles/openxr-example.dir/src/RenderProcess.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/RenderProcess.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/RenderTarget.cpp" "CMakeFiles/openxr-example.dir/src/RenderTarget.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/RenderTarget.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Renderer.cpp" "CMakeFiles/openxr-example.dir/src/Renderer.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Renderer.cpp.o.d"
+  "/Users/cadindie/openxr-vulkan-example-macos/src/Util.cpp" "CMakeFiles/openxr-example.dir/src/Util.cpp.o" "gcc" "CMakeFiles/openxr-example.dir/src/Util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/openxr-example.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openxr-example.dir/src/Buffer.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Buffer.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Context.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Context.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Headset.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Headset.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Main.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Main.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/MirrorView.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/MirrorView.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Pipeline.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Pipeline.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/RenderProcess.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/RenderProcess.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/RenderTarget.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/RenderTarget.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Renderer.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/openxr-example.dir/src/Util.cpp.o"
+  "CMakeFiles/openxr-example.dir/src/Util.cpp.o.d"
+  "openxr-example"
+  "openxr-example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openxr-example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
